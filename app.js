317ed9ad4f1d34d6913ef222bc50f67bce04f9d2
rainmaker514 <tdoug016@fiu.edu>
2023-05-05 22:13:15 -0400

    finished home page

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
+var typed = new Typed(".typed", {
+    strings: ["Fullstack Web Developer", "Data Analyst", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
 
 //display mobile menu
 const mobileMenu = () => {
@@ -8,3 +15,4 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
