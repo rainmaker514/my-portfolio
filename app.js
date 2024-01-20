e208fede535b52cf1f72e013e7c723d710f8a8f7
rainmaker514 <tdoug016@fiu.edu>
2024-01-19 22:29:04 -0500

    added typedjs and scroll reveal animations, changed section title styling

--- app.js
+++ app.js
@@ -13,9 +13,34 @@ navbarLinks.forEach((navbarLink) => {
 
 // TYPING EFFECT
 var typed = new Typed(".typed", {
-strings: ["Frontend", "Backend", "Fullstack"],
-typeSpeed: 100,
-backSpeed: 80,
-backDelay: 2000,
-loop: true
+  strings: ["Frontend", "Backend", "Fullstack"],
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000,
+  loop: true,
 });
+
+// SCROLL REVEAL INIT
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+//SR HERO
+sr.reveal(".hero-img", { delay: 700 });
+sr.reveal(".hero-title", { delay: 500 });
+sr.reveal(".hero-subtext", { delay: 300 });
+sr.reveal(".resume-btn", { delay: 100 });
+
+//SR SECTION TITLES
+sr.reveal(".section-title", { delay: 100 });
+sr.reveal(".border-container", { delay: 300, origin: "left" });
+
+//SR ABOUT
+sr.reveal(".about-text p:nth-child(1)", { delay: 100, origin: "bottom" });
+sr.reveal(".about-text p:nth-child(2)", { delay: 100, origin: "bottom" });
+sr.reveal(".about-text p:nth-child(3)", { delay: 100, origin: "bottom" });
+sr.reveal(".about-text p:nth-child(4)", { delay: 100, origin: "bottom" });
+sr.reveal(".about-icon", { delay: 100, origin: "bottom" });
