e51f10f6f13259270a6a2d89546c5680b7060fa8
rainmaker514 <tdoug016@fiu.edu>
2024-08-09 15:33:19 -0400

    test

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
-const navbarLinks = document.querySelectorAll(".navbar-links");
-const navbarMenu = document.getElementById("navbar-menu");
+// const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
+// const navbarLinks = document.querySelectorAll(".navbar-links");
+// const navbarMenu = document.getElementById("navbar-menu");
 
-//COLLAPSES NAVBAR-MENU WHEN IN MOBILE VIEW AND MOBILE-MENU-TOGGLE CHECKBOX IS CHECKED
-navbarLinks.forEach((navbarLink) => {
-  navbarLink.addEventListener("click", () => {
-    if (window.innerWidth < 960 && mobileMenuToggle.checked == true) {
-      mobileMenuToggle.checked = false;
-    }
-  });
-});
+// //COLLAPSES NAVBAR-MENU WHEN IN MOBILE VIEW AND MOBILE-MENU-TOGGLE CHECKBOX IS CHECKED
+// navbarLinks.forEach((navbarLink) => {
+//   navbarLink.addEventListener("click", () => {
+//     if (window.innerWidth < 960 && mobileMenuToggle.checked == true) {
+//       mobileMenuToggle.checked = false;
+//     }
+//   });
+// });
 
 // TYPING EFFECT
 var typed = new Typed(".typed", {
