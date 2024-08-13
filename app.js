b5e0ccbb821dc2e483fb7206145bd7875eebc912
rainmaker514 <tdoug016@fiu.edu>
2024-08-12 23:36:18 -0400

    test

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-// const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
-// const navbarLinks = document.querySelectorAll(".navbar-links");
-// const navbarMenu = document.getElementById("navbar-menu");
 
-// //COLLAPSES NAVBAR-MENU WHEN IN MOBILE VIEW AND MOBILE-MENU-TOGGLE CHECKBOX IS CHECKED
-// navbarLinks.forEach((navbarLink) => {
-//   navbarLink.addEventListener("click", () => {
-//     if (window.innerWidth < 960 && mobileMenuToggle.checked == true) {
-//       mobileMenuToggle.checked = false;
-//     }
-//   });
-// });
 
 // TYPING EFFECT
 var typed = new Typed(".typed", {
@@ -34,23 +23,5 @@ sr.reveal(".hero-title", { delay: 500 });
 sr.reveal(".hero-subtext", { delay: 300 });
 sr.reveal(".resume-btn", { delay: 100 });
 
-//SR SECTION TITLES
-sr.reveal(".section-title", { delay: 100 });
-sr.reveal(".border-container", { delay: 300, origin: "left" });
 
-//SR ABOUT
-sr.reveal(".about-text p:nth-child(1)", { delay: 100, origin: "bottom" });
-sr.reveal(".about-text p:nth-child(2)", { delay: 100, origin: "bottom" });
-sr.reveal(".about-text p:nth-child(3)", { delay: 100, origin: "bottom" });
-sr.reveal(".about-text p:nth-child(4)", { delay: 100, origin: "bottom" });
-sr.reveal(".about-icon", { delay: 100, origin: "bottom" });
-
-//SR SKILLS
-sr.reveal(".skills-subsection", { delay: 100, origin: "bottom" });
-
-//SR PROJECTS
-sr.reveal(".projects-item", { delay: 100, origin: "bottom" });
-
-//SR CONTACT
-sr.reveal(".contact-form", { delay: 100, origin: "bottom" });
 
